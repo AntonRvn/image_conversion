49e3c804d5b711c055eff59875341a5308a6f58d
Anton <antoharevin@gmail.com>
2025-07-24 15:47:44 +0300

    Добавлена PostgreSQL + SQLAlchemy, модель User

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2252262446@localhost:5432/cartoonify_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from app import routes, models
 
-from app import routes
-
-if __name__ == '__main__':
-    app.run(debug=True)  # Только для локального запуска
+#if __name__ == '__main__':
+#    app.run(debug=True)  # Только для локального запуска
