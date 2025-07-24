92596ac7a64beb2fa9bca308311482ffd5406697
Anton <antoharevin@gmail.com>
2025-07-24 16:05:26 +0300

    Реализована аутентификация

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2252262446@localhost:5432/cartoonify_db'
@@ -8,6 +10,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+@login.user_loader
+def login_user(id):
+    return User.query.get(int(id))
+
 from app import routes, models
 
 #if __name__ == '__main__':
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,35 @@
-from app import app  # Импортируем экземпляр app из __init__.py
+from flask import render_template, redirect, url_for, flash
+from app import app, db
+from app.forms import RegistrationForm, LoginForm
+from app.models import User
+from flask_login import login_user, logout_user, current_user
 
-@app.route('/')
-def home():
-    return "Hello World!"
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('home'))
+        flash('Invalid username or password')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
